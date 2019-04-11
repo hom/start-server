b9a5b881edf3b919497015a5d44400b500c2eef1
mengjun <813935532@qq.com>
2019-04-11 10:26:36 +0800

    add route GET /api/card/:objectId

--- src/routes/index.js
+++ src/routes/index.js
@@ -37,4 +37,34 @@ router.get('/api/cards', async (req, res, next) => {
   });
 })
 
+router.get('/api/card/:objectId', async (req, res, next) => {
+  let where = { ...req.query };
+  let objectId = req.params.objectId;
+
+  let card;
+  try {
+    card = await (new Parse.Query('Card')).get(objectId);
+  } catch (error) {
+    return res.status(500).json({
+      error: '查询Card出错啦',
+    });
+  }
+
+  let marks;
+  try {
+    marks = await (new Parse.Query('Mark')).equalTo('card', card).find();
+  } catch (error) {
+    return res.status(500).json({
+      error: '查询Mark出错啦',
+    });
+  }
+
+  let cardObject = { ...card.toJSON() };
+  cardObject['marks'] = marks.map((item) => item.toJSON());
+
+  return res.status(200).json({
+    data: cardObject,
+  });
+})
+
 module.exports = router;
